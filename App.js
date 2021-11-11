0530453caaf5d29a1752d8249bbddc0860a83dac
Rodrigo Avelino <rodrigo.avelino22@gmail.com>
2021-11-11 19:52:34 -0300

    new icons added to te nav bar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React ,{ useState } from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button,Image } from 'react-native';
 import { NavigationContainer,useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -11,19 +11,30 @@ import {Input} from './components/input'
 //telas principais
 const Tab = createBottomTabNavigator();
 
-const novoGasto = () =>{
-  return(
-    <View style={styles.layout} >
-      <StatusBar style='light'/>
-      <Text style={styles.title} >insira um novo gasto</Text>
-    </View>
-  )
-}
 const resumoDeGastos = () =>{
   return(
     <View style={styles.layout} >
       <StatusBar style='light'/>
-      <Text style={styles.title} >Resumo de gastos por mes</Text>
+      <Text style={styles.title} >resumoDeGastos</Text>
+    </View>
+  )
+}
+const novoGasto = () =>{
+  const [value,setValue] = useState("");
+  return(
+    <View style={styles.layout} >
+      <StatusBar style='light'/>
+      <InputWithLabel
+        style={styles.title}
+        label='Inserir um novo gasto'
+        placeholder='Valor'
+        value={value}
+        onChangeText={setValue}
+        inputStyle={styles.login}
+      />
+      <View style={styles.button}>
+        <Button title="Adicionar" onPress={()=> {}} color="#757575"/>
+      </View>
     </View>
   )
 }
@@ -31,8 +42,34 @@ const resumoDeGastos = () =>{
 const MainNavigator = () =>{
   return(
     <Tab.Navigator >
-      <Tab.Screen name="novoGasto" component={novoGasto} options={{ headerShown: false }}/>
-      <Tab.Screen name="resumoDeGastos" component={resumoDeGastos} options={{ headerShown: false }}/>
+      <Tab.Screen name="novoGasto" component={novoGasto} options={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIcon: ({focused}) => (
+          <View style={{alignItems:'center',justifyContent:'center',top:0}}>
+            <Image
+              source={require('./assets/cash-icon-png-24.jpg')}
+              resizeMode = 'contain'
+              style={{width:25,height:25,tintColor: focused ? 'red' : '#757575'}}
+            />
+            <Text style={{color: focused ? 'red': '#757575',fontSize:12}}>Novo Gasto</Text>
+          </View>
+        ),
+      }} />
+      <Tab.Screen name="resumoDeGastos" component={resumoDeGastos} options={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIcon: ({focused}) => (
+          <View style={{alignItems:'center',justifyContent:'center',top:0}}>
+            <Image
+              source={require('./assets/6f3ee02c67912c31632335bba755dd1d.png')}
+              resizeMode = 'contain'
+              style={{width:25,height:25,tintColor: focused ? 'red' : '#757575'}}
+            />
+            <Text style={{color: focused ? 'red': '#757575',fontSize:12}}>Resumo</Text>
+          </View>
+        ),
+      }} />
     </Tab.Navigator>
   )
 }
@@ -47,8 +84,10 @@ const initScreen = () =>{
   const navigation = useNavigation();
   const [login,setLogin] = useState("");
   const [password,setPassword] = useState("");
+  const logo = require('./assets/pngwing.com.png')
   return(
     <View style={styles.layout}>
+      <Image style={styles.image} source={logo}/>
       <InputWithLabel
         style={styles.title}
         label='Sing In'
@@ -65,7 +104,9 @@ const initScreen = () =>{
         inputStyle={styles.login}
         secureTextEntry
       />
-      <Button title="Login" onPress={()=>navigation.navigate('Main')}/>
+      <View style={styles.button}>
+        <Button title="Login" onPress={()=>navigation.navigate('Main')} color="#757575"/>
+      </View>
     </View>
   )
 }
@@ -90,6 +131,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 8,
     backgroundColor: '#c2c2c2',
+    width:'100%',
   },
   title: {
     margin: 24,
@@ -100,6 +142,16 @@ const styles = StyleSheet.create({
   },
   login: {
     borderColor:'#FFFFFF',
-    borderWidth:2
+    borderWidth:2,
+    borderRadius:10
+  },
+  button:{
+    paddingHorizontal:100,
+    marginTop:100
+  },
+  image: {
+    width:100,
+    height:100,
+    alignSelf:'center'
   }
 });
