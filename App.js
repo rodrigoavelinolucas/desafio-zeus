68b9df9b8012648b3fce5393b44d99cffb19e599
Rodrigo Avelino <rodrigo.avelino22@gmail.com>
2021-11-11 15:53:38 -0300

    telas criadas

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { NavigationContainer,useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+
+//telas principais
+const Tab = createBottomTabNavigator();
+
+const novoGasto = () =>{
+  return(
+    <View style={styles.layout} >
+      <StatusBar style="light" />
+      <Text style={styles.title} >insira um novo gasto</Text>
+    </View>
+  )
+}
+const resumoDeGastos = () =>{
+  return(
+    <View style={styles.layout} >
+      <Text style={styles.title} >Resumo de gastos por mes</Text>
+    </View>
+  )
+}
+
+const MainNavigator = () =>{
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="novoGasto" component={novoGasto}/>
+      <Tab.Screen name="resumoDeGastos" component={resumoDeGastos}/>
+    </Tab.Navigator>
+  )
+}
+
+/*
+*/
+
+//telas iniciais
+const Stack = createNativeStackNavigator()
+
+const initScreen = () =>{
+  const navigation = useNavigation();
+  return(
+    <View style={styles.layout}>
+      <Text style={styles.title} >initScreen</Text>
+      <Button title="Login" onPress={()=>navigation.navigate('Main')}/>
+    </View>
+  )
+}
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="Init" component={initScreen}/>
+        <Stack.Screen name="Main" component={MainNavigator}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
+    backgroundColor: '#4181e8',
+  },
+  title: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color:'#FFFFFF'
   },
 });
